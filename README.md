ed2c9435568d844650f259a679e6859bdef4790d
Anthony Hoisington <98637198+ALLSTARETC123@users.noreply.github.com>
2026-06-19 17:44:58 -0700

    Updated package.json

--- README.md
+++
@@ -1,3 +0,0 @@
-# bolt_poly_profit_engine
-
-[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-nikou1vb)
--- src/engine.ts
+++
@@ -1,8 +0,0 @@
-// src/engine.ts
-import { runHeadlessEngine } from './lib/scanner-engine.js';
-import { executeTransaction } from './lib/executor.js';
-
-runHeadlessEngine(async (tx, result) => {
-  console.log("Profitable route identified:", result.profitEstimate);
-  await executeTransaction(tx);
-});
